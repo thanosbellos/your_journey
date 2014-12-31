368138b19ba1b0745b0e127c854a3b2e7c92d707
Thanos Bellos <thanosbellos@me.com>
2014-12-31 19:51:17 +0200

    Add user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,14 @@ RSpec.describe UsersController, :type => :controller do
       get :new
       expect(response).to have_http_status(:success)
     end
+
+    it "assigns a new User to @user" do
+
+    end
+    it "renders the :new template" do
+
+    end
+
   end
 
 end
