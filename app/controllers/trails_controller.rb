7ad74a326307ebc39918df19f7d51afa0f23ae5a
Thanos Bellos <thanosbellos@me.com>
2015-01-22 00:51:24 +0200

    Add new create and show actions and views for trails

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -4,6 +4,23 @@ class TrailsController < ApplicationController
   end
 
   def new
-
+    @trail = Trail.new
   end
+
+  def create
+    @trail = Trail.new(trail_params)
+    p @trail
+    if @trail.save
+      render 'show'
+    else
+      p @trail.errors.full_messages
+      render 'new'
+    end
+  end
+
+
+  private
+    def trail_params
+      params.require(:trail).permit(:name , :start_point , :end_point , :length , :duration , :travel_by , :difficulty , :rating )
+    end
 end
