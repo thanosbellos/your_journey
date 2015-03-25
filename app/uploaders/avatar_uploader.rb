665bef623b75c23aa1a66b43d3b3ea4c5c9bf015
Thanos Bellos <thanosbellos@me.com>
2015-03-25 18:17:03 +0200

    Add default image when user does not upload Changed scale of uploaded photos

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -17,12 +17,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+   def default_url
+  #   # For Rails 3.1+ asset pipeline compatibility:     #ActionController::Base.helpers.asset_path("app/assets/images/fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+     "fallback/" + [version_name, "default.png"].compact.join('_')
+   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
@@ -33,11 +32,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_fit => [36,36]
+     process :resize_to_fit => [32,32]
    end
 
    version :rounded do
-     process :resize_to_fit => [104,104]
+     process :resize_to_fit => [96,96]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
