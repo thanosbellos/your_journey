d64639c332247279fab5697c824f4a1a39f43714
Thanos Bellos <thanosbellos@me.com>
2015-03-25 11:32:51 +0200

    edit user profile without password if user has registered with fb

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 feature 'User profile' , focus:true do
-  scenario 'a user can edit his profile' do
+  scenario 'a user can edit his profile info without providing password' do
     user = create(:user)
     sign_in user
     visit user_path(user)
     click_link 'Edit your profile'
+    fill
   end
 
 
