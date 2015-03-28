8fd588e3ca7384ef2c10fcf6fb151690bbc6fc8b
Thanos Bellos <thanosbellos@me.com>
2015-03-28 20:32:01 +0200

    Add activities and its association to users and trails

---
+++ app/models/activities_inserts.rb
@@ -0,0 +1,17 @@
+r = Activity.new(:name => "Offroad driving")
+r.save
+r = Activity.new(:name => "Rock Climbing")
+r.save
+r = Activity.new(:name => "Scenic Driving")
+r.save
+r = Activity.new(:name => "Walking")
+r.save
+r = Activity.new(:name => "Trail Running")
+r.save
+r = Activity.new(:name => "Hiking")
+r.save
+r = Activity.new(:name => "Mountain Biking")
+r.save
+r = Activity.new(:name => "Motorcycling")
+r.save
+
