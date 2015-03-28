f1514836e762d226f1512e8509aeb9e3f6571c29
Thanos Bellos <thanosbellos@me.com>
2015-03-28 12:52:08 +0200

    Fix links on header nav bar

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module YourJourney
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.serve_static_files = false
     config.generators do |g|
       g.test_framework :rspec,
         fixtures: true,
