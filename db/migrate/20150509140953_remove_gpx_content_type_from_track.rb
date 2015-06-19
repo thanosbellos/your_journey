164444e67b41f4cbf63ded8bcb0f5f40f3a84ff9
Thanos Bellos <thanosbellos@me.com>
2015-06-19 14:48:14 +0300

    Remove migrations and models

--- db/migrate/20150509140953_remove_gpx_content_type_from_track.rb
+++
@@ -1,5 +0,0 @@
-class RemoveGpxContentTypeFromTrack < ActiveRecord::Migration
-  def change
-    remove_column :tracks , :gpx_content_type
-  end
-end
--- db/migrate/20150531223913_add_track_segments.rb
+++
@@ -1,9 +0,0 @@
-class AddTrackSegments < ActiveRecord::Migration
-  def change
-    create_table :tracksegments do |t|
-      t.references :track, index: true, foreign_key: true
-      t.line_string :tracksegment_path , srid: 3785
-      t.timestamps null: false
-    end
-  end
-end
