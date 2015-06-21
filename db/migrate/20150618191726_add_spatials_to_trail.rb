3194cb52275bc1fc8edcb91b5ca40a58a3bf6dee
Thanos Bellos <thanosbellos@me.com>
2015-06-21 20:42:31 +0300

    Change columns type

--- db/migrate/20150618191726_add_spatials_to_trail.rb
+++ db/migrate/20150618191726_add_spatials_to_trail.rb
@@ -1,8 +1,8 @@
 class AddSpatialsToTrail < ActiveRecord::Migration
   def change
-    add_column :trails , :trail_path ,:line_string, srid: 3785
-    add_column :trails , :origin_point , :st_point, srid: 3785
-    add_column :trails , :destination_point , :st_point, srid:3785
+    add_column :trails , :trail_path ,:line_string, srid: 3875
+    add_column :trails , :origin_point , :st_point, srid: 3875
+    add_column :trails , :destination_point , :st_point, srid:3875
     add_attachment :trails , :gpx
     add_column  :trails,  :trailgeometry , :string
   end
